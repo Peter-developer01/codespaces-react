ceaae1666c1693649bea0fedd863ca2457497c6c
Developer Peter <peter.kolosov.developer@yandex.ru>
2023-03-06 15:35:00 +0000

    fix

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
+        {/* <img src="Octocat.png" className="App-logo" alt="logo" /> */}
         <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
+          GitHub Pages <span className="heart"><s>with</s> <u>by</u></span> React
         </p>
         <p className="small">
           Edit <code>src/App.js</code> and save to reload.
@@ -14,11 +14,11 @@ function App() {
         <p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://peter.developer02.github.io"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn
           </a>
         </p>
       </header>
